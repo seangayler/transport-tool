8807731b24edf35a5a26cdca2f411d920e5b7989
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-07-12 15:26:32 +1000

    Created accounts app.

---
+++ website/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def login(request):
+  return HttpResponse()
+
+def register(request):
+  return HttpResponse()
