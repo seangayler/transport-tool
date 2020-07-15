64f309d5114853ef166124b102ead1aa72bda152
Sean Gayler <scianciogayl@student.unimelb.edu.au>
2020-07-15 15:16:56 +1000

    Added Django UserCreationForm and the map app.

--- website/register/views.py
+++ website/register/views.py
@@ -6,7 +6,18 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    return render(request, 'register/index.html')
+    context = {}
+    form = UserCreationForm(None)
+    context['form'] = form
+    return render(request, 'register/index.html', context)
 
 def create_user(request):
     context = {}
+    form = UserCreationForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return render(request,'accounts/index.html')
+    context['form']=form
+    return render(request,'registration/sign_up.html',context)
